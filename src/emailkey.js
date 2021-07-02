4cd5abf0d716713c1b2730fc32ad79b18f1d84a1
ec19135 <ec19135@qmul.ac.uk>
2021-07-02 19:02:51 +0100

    email set up with emailjs

---
+++ src/emailkey.js
@@ -0,0 +1,4 @@
+export default {
+    USER_ID: 'user_3iEqxrUZkT3SQY2XiLcKj',
+    TEMPLATE_ID: '5dce125eed0a851e6136f1aa3a61e776',
+}
--- src/pages/Socials.js
+++ src/pages/Socials.js
@@ -1,10 +1,37 @@
 import React from 'react';
 import {Container} from 'react-bootstrap';
+import emailjs from 'emailjs-com';
+import{ init } from 'emailjs-com';
+
+
+function sendEmail(e) {
+    e.preventDefault();
+
+    emailjs.sendForm('service_97jan3n', 'template_au856ip', e.target, 'user_3iEqxrUZkT3SQY2XiLcKj')
+      .then((result) => {
+          console.log(result.text);
+      }, (error) => {
+          console.log(error.text);
+      });
+  }
 
 const Socials = () => (
+
+
     <div className="body medium-text">
         <Container>
             <p>Louie just put ur socials here and the ability to send emails to ur email.</p>
+            <form className="contact-form" onSubmit={sendEmail}>
+            <label>Name</label>
+            <input type="text" name="name" />
+            <label>Email</label>
+            <input type="text" name="email" />
+            <label>Phone Number</label>
+            <input type="tel" name="phone" />
+            <label>Message</label>
+            <textarea name="message" />
+            <input type="submit" value="Send" />
+            </form>
         </Container>
     </div>
 
