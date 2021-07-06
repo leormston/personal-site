ca085fd6daaeb5f9f985103ae95551c9d92e0258
leormston <l.e.ormston@gmail.com>
2021-07-06 23:15:10 +0100

    progress on contact page has been made

--- src/emailkey.js
+++ src/emailkey.js
@@ -1,4 +1,4 @@
 export default {
-    USER_ID: 'user_3iEqxrUZkT3SQY2XiLcKj',
+    USER_ID: 'vv',
     TEMPLATE_ID: '5dce125eed0a851e6136f1aa3a61e776',
 }
--- src/pages/Socials.js
+++ src/pages/Socials.js
@@ -1,8 +1,11 @@
 import React from 'react';
-import {Container} from 'react-bootstrap';
+import {Row, Col, Container} from 'react-bootstrap';
 import emailjs from 'emailjs-com';
 import{ init } from 'emailjs-com';
-
+import { faPhone, faEnvelope } from "@fortawesome/free-solid-svg-icons";
+import { faLinkedin } from '@fortawesome/free-brands-svg-icons';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+<script src="https://kit.fontawesome.com/2334c2b59b.js" crossorigin="anonymous"></script>
 
 function sendEmail(e) {
     e.preventDefault();
@@ -20,18 +23,82 @@ const Socials = () => (
 
     <div className="body medium-text">
         <Container>
-            <p>Louie just put ur socials here and the ability to send emails to ur email.</p>
+            <h2 className="border-pink">Feel free to send me a message!</h2>
             <form className="contact-form" onSubmit={sendEmail}>
-            <label>Name</label>
-            <input type="text" name="name" />
-            <label>Email</label>
-            <input type="text" name="email" />
-            <label>Phone Number</label>
-            <input type="tel" name="phone" />
-            <label>Message</label>
-            <textarea name="message" />
-            <input type="submit" value="Send" />
+           <Row>
+            
+                <Col md={4} sm={12}>
+                    <label className="form-label">Name</label>
+                    <br/>
+                    <input  className="form-input" type="text" name="name" />   
+                </Col>
+                <Col md={4} sm={12}>
+                    <label className="form-label">Email</label>
+                    <br/>
+                    <input className="form-input" type="text" name="email" />  
+                </Col>
+                <Col md={4} sm={12}>
+                    <label className="form-label">Phone Number</label>
+                    <br/>
+                    <input className="form-input" type="tel" name="phone" />   
+                </Col>
+                <Col md={12}>
+                    <label className="form-label">Message</label>
+                    <br />
+                    <textarea className = "form-input" name="message" /> 
+                </Col>
+                <Col md={12}>
+                    <input className="form-button" type="submit" value="Send Message" />    
+                </Col>
+            
+                
+            </Row>
             </form>
+            <br/>
+            <h2 className="border-pink">Or get in touch another way</h2>
+            <Row>
+                <Col>
+                    <Row>
+                        <Col xs={2}>
+                            <FontAwesomeIcon icon={faPhone} />
+                        </Col>
+                        <Col>
+                            <p>(+44) 07471 479813</p>
+                        </Col>
+                        
+                    </Row>
+                    <Row>
+                        <Col xs={2}>
+                            <FontAwesomeIcon icon={faEnvelope} />
+                        </Col>
+                        <Col>
+                            <p>l.e.ormston@gmail.com</p>
+                        </Col>
+                        
+                    </Row>
+                    <a className= "nostyle-link" href="https://www.linkedin.com/in/louie-ormston-48810a150/" >
+                    <Row>
+                        
+                        <Col xs={2}>
+                            <FontAwesomeIcon icon={faLinkedin} />
+                        </Col>
+                        <Col>
+                            <p>Check out my LinkedIn Profile</p>
+                        </Col>
+                        
+                    </Row>
+                    </a>
+                </Col>
+                <Col>
+                    <p>Email - l.e.ormston@gmail.com</p>
+                </Col>
+                <Col>
+                    <p> Instagram - l.e.ormston@gmail.com </p>
+                </Col>
+            </Row>
+
+            
+            
         </Container>
     </div>
 
